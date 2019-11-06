6e7a3ea26aae104e16b4908d6784b6d47852a8bd
punitha86 <punith.job@gmail.com>
2019-11-06 09:50:58 -0800

    edit route implementation

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -19,7 +19,17 @@ router.post('/', (req, res) => {
     console.log(createdNewBlog);
   });
 })
-
+///after clicking edit it should show all the correct details
+router.get('/:id/edit', (req, res) => {
+    Blog.findById(req.params.id, (error, editBlog) => {
+        res.render(
+            'blogs/edit.ejs',
+            {
+                blog:editBlog
+            }
+        );
+    });
+});
 
 
 module.exports = router;
