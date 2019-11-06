134c3e0b9d9b4afa62ba1640d1330b408a5df07b
punitha86 <punith.job@gmail.com>
2019-11-05 21:59:32 -0800

    building the basic bones

--- server.js
+++ server.js
@@ -12,29 +12,33 @@ require('dotenv').config();
 /////**********************************//////////
 const PORT = process.env.PORT;
 console.log(PORT);
+///middleware////
+app.use(express.urlencoded({ extended: true }));
 /////**********************************//////////
 /////*************Database***********//////////
 /////**********************************//////////
 const MONGODB_URI = process.env.MONGODB_Uri;
 console.log(MONGODB_URI);
+
+
+//route
+app.get('/' , (req, res) => {
+  res.render('home.ejs');
+  //res.send('Hello World!');
+});
 // Connect to Mongo &
 // Fix Depreciation Warnings from Mongoose
 // May or may not need these depending on your Mongoose version
 mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
 );
-///middleware////
-app.use(express.urlencoded({ extended: false }));
+
 
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
-//route
-app.get('/' , (req, res) => {
-  res.redirect("home.ejs")
-  //res.send('Hello World!');
-});
+
 
 ////PORT
 //app.listen(PORT, () => console.log( 'Listening on port:', PORT));
